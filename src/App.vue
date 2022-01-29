e93d91b7a09df1ae48faaa0a9d777dbd73d97032
AlexyShu <Za9i@bk.ru>
2022-01-29 11:27:27 +0300

    home work 1: поиск имени по массиву (computed, v-on, v-for)

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <!-- <MyComponent /> -->
-    <Lesence1 />
+    <!-- <Lesence1 /> -->
+    <HW1 />
   </div>
 </template>
 
 <script>
 // import MyComponent from "./components/MyComponent";
-import Lesence1 from "./components/Lesence1";
+// import Lesence1 from "./components/Lesence1";
+import HW1 from "./components/HW1";
 
 export default {
   name: 'App',
   components: {
     // MyComponent
-    Lesence1
+    // Lesence1
+    HW1
   }
 }
 </script>
---
+++ src/components/HW1.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="conatiner">
+    <h2>Я найду тебя!</h2>
+    <input type="text" v-on:input="searchText = $event.target.value">
+    <p>Всего имен {{ names.length }}, совпадений: {{ getFilteredNames.length }}</p>
+    <ul class="list">
+      <li
+        v-for="(name, index) in getFilteredNames"
+        :key="index"
+      >
+       {{name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HW1",
+    data() {
+        return {
+            searchText: "",
+            names: ["Данил", "Василий", "Дмитрий", "Григорий", "Максим"]
+        }
+    },
+    computed: {
+        getFilteredNames() {
+            return this.names.filter(name => name.toLowerCase().includes(this.searchText.toLowerCase()))
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 14px;
+        margin-top: 40px;
+    }
+    ul {
+         list-style-type: none;
+         margin: 0;
+         padding: 0;
+    }
+</style>
